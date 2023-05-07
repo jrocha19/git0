0ea088e31f3d11c3c15a1a6379d9ca83ae3fd45c
joel <jrocha19@gmail.com>
2023-05-07 10:54:54 +0200

    Revert "Header"
    This reverts commit 5d14ed2edcec6ca63d16abdcc9cb8e0f65a92b51.

--- Program.java
+++ Program.java
@@ -4,7 +4,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 Joel Rocha
+ * Copyright 2020 Joel Rocha <jrocha19@gmail.com>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
